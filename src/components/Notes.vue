7cad124a423a5fc9e4c220eab6ef5f8f84c9e2df
Bekhzod <stx.webdev@gmail.com>
2023-11-25 12:51:47 +0500

    apply dynamic toggling of css classes

--- src/components/Notes.vue
+++ src/components/Notes.vue
@@ -1,7 +1,12 @@
 <template>
   <div class="notes">
-    <div class="note" v-for="(note, index) in notes" :key="index">
-      <div class="note-header">
+    <div
+      class="note"
+      :class="{ full: !grid }"
+      v-for="(note, index) in notes"
+      :key="index"
+    >
+      <div class="note-header" :class="{ full: !grid }">
         <p>{{ note.title }}</p>
         <button @click="removeNote(index)" class="close-btn">X</button>
       </div>
@@ -19,6 +24,10 @@ export default {
   props: {
     notes: {
       type: Array,
+      required: true,
+    },
+    grid: {
+      type: Boolean,
       required: true,
     },
   },
@@ -44,18 +53,57 @@ export default {
   padding: 18px 20px;
   margin-bottom: 20px;
   background-color: #fff;
+  transition: all 0.25s cubic-bezier(0.02, 0.01, 0.47, 1);
+  box-shadow: 0 30px 30px rgba(0, 0, 0, 0.02);
+
+  &:hover {
+    box-shadow: 0 30px 30px rgba(0, 0, 0, 0.04);
+    transform: translate(9, -6px);
+    transition-delay: 0s !important;
+  }
+
+  &.full {
+    width: 100%;
+    text-align: center;
+  }
 }
 
 .note-header {
   display: flex;
   align-items: center;
   justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
   p {
     font-size: 22px;
     color: #999;
   }
   .close-btn {
     cursor: pointer;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999;
+    cursor: pointer;
+
+    &.active {
+      color: #402caf;
+    }
+
+    &:last-child {
+      margin-right: 0;
+    }
+  }
+
+  &.full {
+    justify-content: center;
+    p {
+      margin-right: 16px;
+      &:last-child {
+        margin-right: 0;
+      }
+    }
   }
 }
 
